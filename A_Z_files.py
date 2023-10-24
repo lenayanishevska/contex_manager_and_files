e5840a0c5f26e6a68a836b4c691cc356d960e58e
Olena Yanishevska <olena.yanishevska.pz.2021@lpnu.ua>
2023-10-24 11:31:52 +0300

    new files folder

--- A_Z_files.py
+++ A_Z_files.py
@@ -5,12 +5,12 @@ def from_A_to_Z():
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
     for i in alphabet:
-        file_name = i + ".txt"
+        file_name = "files/" + i + ".txt"
         with open(file_name, "w") as file:
             file.write(str(random.randint(1, 100)))
 
     with open('summary.txt', 'w') as summary:
         for i in alphabet:
-            filename = i + '.txt'
+            filename = "files/" + i + '.txt'
             with open(filename, 'r') as file:
                 summary.write(f"{filename}: {int(file.read())}\n")
